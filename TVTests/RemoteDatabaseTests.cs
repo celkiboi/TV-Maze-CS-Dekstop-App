63796935fa669c5b78f0313dc93f1944f7e4c354
Tomislav Celic <celer.tomislav@gmail.com>
2023-07-30 19:41:00 +0200

    Adressing some issues about RemoteDatabaseTests

--- TVTests/RemoteDatabaseTests.cs
+++ TVTests/RemoteDatabaseTests.cs
@@ -12,6 +12,12 @@ namespace TVTests;
 [TestFixture]
 internal class RemoteDatabaseTests
 {
+    // This test needs to be re-done
+    // The issue comes from the fact that the test results are volatile
+    // The return can sometimes have a different weight
+    // TL;DR: data that is being fetch needs to be static, but isn't
+    // For now, simply change the "weight" in TVApp/TVTests/bin/Debug/net7.0 to match
+
     [Test]
     public void FetchDataAsync_Test()
     {
